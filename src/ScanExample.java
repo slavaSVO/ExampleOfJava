431c2d8b16d67a051466dff2bb3a68be65a0328d
slavaSVO <slavaSVO@gmail.com>
2020-01-16 10:42:43 +0200

    Scanner in process.

--- src/ScanExample.java
+++ src/ScanExample.java
@@ -1,4 +1,28 @@
 import java.util.Scanner;
+
 public class ScanExample {
-    Scanner scan = new Scanner(readFile);
+    private Scanner sysScan = new Scanner(System.in);//Сканер який зчитує дані введені в консолі
+    private Scanner txtScan = new Scanner("Люблю тебя, Петра творенье,\n" +
+            "Люблю твой строгий, стройный вид,\n" +
+            "Невы державное теченье,\n" +
+            "Береговой ее гранит.\n" +
+            "1812год");//Сканер який зчитує дані із текстового рядку передагого йому як параметр
+
+    public void searchInText() {
+        while (txtScan.hasNext()){
+            String line = txtScan.nextLine();
+            System.out.println(line);
+        }
+    }
+
+    public void searchInSystemScan (){
+        System.out.println("Введіть ціле число");
+        int nInt;
+        nInt = sysScan.nextInt();
+        System.out.println("Введіть числи в форматі real (x.y)");
+        double nDouble;
+        nDouble = sysScan.nextDouble();
+    }
+
+
 }
